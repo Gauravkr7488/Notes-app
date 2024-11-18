9d36f3d30fa51b4e15cea3bf16ae112c595371a0
Gaurav Kumar <gauravkr748800@gmail.com>
2024-11-18 21:58:11 +0530

    added delete button

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 const create = document.getElementById("create");
-const remove = document.getElementsByClassName("del");
 const notearea = document.getElementById("notearea")
 const newnote = document.createElement("div")
-
-
 
 function createnote() {
     const newnote = document.createElement("div")
     newnote.innerHTML = `<textarea name="" id="" class="note" placeholder="Write here..."></textarea>
     <button class="del">delete</button>`;
-    
+
     notearea.appendChild(newnote)
     newnote.classList.add("clipboard");
+
+    const deleteButton = newnote.querySelector(".del");
+    deleteButton.addEventListener("click", function () {
+        newnote.remove();
+    });
 }
 
 create.addEventListener("click", createnote, false)
-
-for (let index = 0; index < remove.length; index++) {
-    remove[index].addEventListener("click", function (e) {
-        e.target.parentelement.remove();
-    }, false)
-}
